61c69ac050527526c2e8970143cc7e6189334e38
Alexander Deschamps <alexadeschamps@gmail.com>
2015-01-10 20:47:25 -0500

    moves left and right with barriers

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var proj_cont = document.querySelector("#proj_cont");
 var project = [project1, project2, project3, project4];
 var mouse = false;
 var curr_x;
+var p_index;
 
 var counter = 0
 project.forEach(function(proj){
@@ -27,31 +28,57 @@ projects.addEventListener("mouseup", function(){
 projects.addEventListener("mousemove", function(e){
 	if(mouse){
 		change = e.clientX - curr_x;
-		// console.log(change);
-		var count = 0;
+		curr_x+=change;
 		var proj1_left = project1.style.left;
 		var proj1_str = proj1_left.replace("px","");
 		var barr = window.parseInt(proj1_str);
-		project.forEach(function(proj){
-			if(barr < 0){ //||
-			 // project1.style.left >= (window.innerWidth.width * project.length) )
-				var percent= proj.style.left.replace("px","");
-				var left = window.parseInt(percent);
-				var newValue = left + change;
-				proj.style.left = newValue + "px";
-				if(count === 0){
-					console.log(percent);
-				}
-				count++;
+		if (change < 0){
+			var proj4_left = project4.style.left;
+			var proj4_str = proj4_left.replace("px","");
+			var barr = window.parseInt(proj4_str);
+			if( (barr + change) <= 0 ){
+				change = -(barr);
 			}
-		});
-		curr_x+=change;
+			// console.log(barr);
+			// console.log(change);
+			if (barr > 0){
+				move(change);
+			}
+			// move(change);
+		}else if (change > 0){
+			var proj1_left = project1.style.left;
+			var proj1_str = proj1_left.replace("px","");
+			var barr = window.parseInt(proj1_str);
+			// console.log(barr + change);
+			if( (barr + change) >= 0 ){
+				change = -(barr);
+			}
+			// console.log(barr);
+			// console.log(change);
+			if (barr < 0){
+				move(change);
+			}
+		}
 	};
 });
 
-var left = function(){
+// if(barr < 0){ //||
+			 // project1.style.left >= (window.innerWidth.width * project.length) )
 
+
+function move(change){
+
+	project.forEach(function(proj){
+		var percent= proj.style.left.replace("px","");
+		var left = window.parseInt(percent);
+		var newValue = left + change;
+		proj.style.left = newValue + "px";
+	});
 };
-var right = function(){
 
-}
+// function autoMove(curr_pos){
+// 	end_pos = 
+// 	for (var i = 0; i <  i++) {
+// 		curr_position
+// 	};
+// }
